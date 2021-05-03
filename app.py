aa6595ee1cfd2d21168e128a99e5c3d895e4cbcc
Airi Taukurade <34795565+Taukurade@users.noreply.github.com>
2021-05-03 20:08:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import random
 import requests
 from io import StringIO
 from random import choice
-
+TEMPLATE_WIDTH = 574
+TEMPLATE_HEIGHT = 522
+TEMPLATE_COORDS = (75, 45, 499, 373)
+PADDING = 10
 app = Flask(__name__)
 def drawXAxisCenteredText(image, text, font, size, pos_y):
     draw = ImageDraw.Draw(image)
@@ -39,10 +42,7 @@ def demote(str1,str2):
     LOWER_SIZE = 14
     LOWER_FONT_Y = 450
 
-    TEMPLATE_WIDTH = 574
-    TEMPLATE_HEIGHT = 522
-    TEMPLATE_COORDS = (75, 45, 499, 373)
-    PADDING = 10
+
 
     imgio=StringIO()
     frame = PIL.Image.open(TEMPLATE_FILENAME)
